8b384763c87927e1a5f3c8ca7c6909a230ae371a
JuanjoG92 <saladasaldia@live.com>
2025-02-08 15:13:23 -0300

    Update SendMessagesSchenduleWbot.ts

--- backend/src/services/WbotServices/SendMessagesSchenduleWbot.ts
+++ backend/src/services/WbotServices/SendMessagesSchenduleWbot.ts
@@ -12,7 +12,7 @@ import SendMessageSystemProxy from "../../helpers/SendMessageSystemProxy";
 const SendMessagesSchenduleWbot = async (): Promise<void> => {
   const currentDate = new Date(
     new Date().toLocaleString("en-US", {
-      process.env.TIMEZONE || "America/Argentina/Buenos_Aires"
+      timeZone: process.env.TIMEZONE || "America/Sao_Paulo"
     })
   );
   const twentyFourHoursAgo = new Date(
