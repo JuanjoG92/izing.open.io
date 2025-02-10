62ca63ced6449a57850eb4f4c453a88c208880d8
JuanjoG92 <saladasaldia@live.com>
2025-02-10 03:47:27 -0300

    Actualización de textos en el modal de etiquetas
    Se ha cambiado el texto del título del modal de "Editar" a "Crear" cuando se está creando una nueva etiqueta.
    Se ha cambiado la etiqueta del campo de entrada de "Nome da Etiqueta" a "Nombre de Etiqueta".
    Se ha cambiado la etiqueta del checkbox de "Ativo" a "Activo".
    Se ha cambiado el texto del botón de "Salvar" a "Guardar".
    Se ha cambiado el mensaje de notificación de "Etiqueta criada!" a "Etiqueta creada!".
    Se ha cambiado el mensaje de error de "Ocorreu um erro ao criar a etiqueta" a "Se produjo un error al crear la etiqueta".

--- frontend/src/pages/etiquetas/ModalEtiqueta.vue
+++ frontend/src/pages/etiquetas/ModalEtiqueta.vue
@@ -9,7 +9,7 @@
       style="width: 500px"
       class="q-pa-lg"
     >
-      <div class="text-h6">{{ etiquetaEdicao.id ? 'Editar': 'Criar' }} Etiqueta</div>
+      <div class="text-h6">{{ etiquetaEdicao.id ? 'Editar': 'Crear' }} Etiqueta</div>
       <q-card-section>
         <q-input
           class="row col"
@@ -17,7 +17,7 @@
           dense
           outlined
           v-model="etiqueta.tag"
-          label="Nome da Etiqueta"
+          label="Nombre de Etiqueta"
         />
         <q-input
           rounded
@@ -55,7 +55,7 @@
         </q-input>
         <q-checkbox
           v-model="etiqueta.isActive"
-          label="Ativo"
+          label="Activo"
         />
       </q-card-section>
       <q-card-actions
@@ -71,7 +71,7 @@
         />
         <q-btn
           rounded
-          label="Salvar"
+          label="Guardar"
           color="positive"
           @click="handleEtiqueta"
         />
@@ -153,7 +153,7 @@ export default {
             type: 'positive',
             progress: true,
             position: 'top',
-            message: 'Etiqueta criada!',
+            message: 'Etiqueta creada!',
             actions: [{
               icon: 'close',
               round: true,
@@ -165,7 +165,7 @@ export default {
         this.fecharModal()
       } catch (error) {
         console.error(error)
-        this.$notificarErro('Ocorreu um erro ao criar a etiqueta', error)
+        this.$notificarErro('Se produjo un error al crear la etiqueta', error)
       }
     }
   }
