83ebc0411838b0317f27958f5813c371734a6dbb
JuanjoG92 <saladasaldia@live.com>
2025-02-14 15:14:44 -0300

    Actualiza mensajes de error a español en erros.js
    Se han traducido los mensajes de error del archivo `erros.js` de portugués a español. Esto incluye errores relacionados con WhatsApp, como la falta de un WhatsApp predeterminado, problemas de inicialización, verificación de contactos y errores de autenticación. Se han eliminado los mensajes en portugués y se han añadido nuevas traducciones en español para mantener la coherencia en el idioma.

--- frontend/src/service/erros.js
+++ frontend/src/service/erros.js
@@ -1,36 +1,36 @@
 const backendErrors = {
   ERR_NO_DEF_WAPP_FOUND:
-    'Nenhum WhatsApp padrão encontrado. Verifique a página de conexões.',
+    'No se encontró un WhatsApp predeterminado. Verifique la página de conexiones.',
   ERR_WAPP_NOT_INITIALIZED:
-    'Esta sessão do WhatsApp não foi inicializada. Verifique a página de conexões.',
+    'Esta sesión de WhatsApp no ha sido inicializada. Verifique la página de conexiones.',
   ERR_WAPP_INITIALIZED:
-    'Não está conectado com o Whatsapp. Estamos reiniciando a conexão. Tente novamente em alguns segundos.',
+    'No está conectado con WhatsApp. Estamos reiniciando la conexión. Intente nuevamente en unos segundos.',
   ERR_WAPP_CHECK_CONTACT:
-    'Não foi possível verificar o contato do WhatsApp. Verifique a página de conexões',
-  ERR_WAPP_INVALID_CONTACT: 'Este não é um número de Whatsapp válido.',
+    'No se pudo verificar el contacto de WhatsApp. Verifique la página de conexiones.',
+  ERR_WAPP_INVALID_CONTACT: 'Este no es un número de WhatsApp válido.',
   ERR_WAPP_DOWNLOAD_MEDIA:
-    'Não foi possível baixar mídia do WhatsApp. Verifique a página de conexões.',
+    'No se pudo descargar el medio de WhatsApp. Verifique la página de conexiones.',
   ERR_INVALID_CREDENTIALS:
-    'Erro de autenticação. Por favor, tente novamente.',
+    'Error de autenticación. Por favor, intente nuevamente.',
   ERR_SENDING_WAPP_MSG:
-    'Erro ao enviar mensagem do WhatsApp. Verifique a página de conexões.',
-  ERR_DELETE_WAPP_MSG: 'Não foi possível excluir a mensagem do WhatsApp.',
-  ERR_OTHER_OPEN_TICKET: 'Já existe um Ticket aberto para este contato.',
-  ERR_SESSION_EXPIRED: 'Sessão expirada. Por favor entre.',
+    'Error al enviar el mensaje de WhatsApp. Verifique la página de conexiones.',
+  ERR_DELETE_WAPP_MSG: 'No se pudo eliminar el mensaje de WhatsApp.',
+  ERR_OTHER_OPEN_TICKET: 'Ya existe un ticket abierto para este contacto.',
+  ERR_SESSION_EXPIRED: 'Sesión expirada. Por favor ingrese nuevamente.',
   ERR_USER_CREATION_DISABLED:
-    'A criação do usuário foi desabilitada pelo administrador.',
-  ERR_NO_PERMISSION: 'Você não tem permissão para acessar este recurso.',
-  ERR_DUPLICATED_CONTACT: 'Já existe um contato com este número.',
-  ERR_NO_SETTING_FOUND: 'Nenhuma configuração encontrada com este ID.',
-  ERR_NO_CONTACT_FOUND: 'Nenhum contato encontrado com este ID.',
-  ERR_NO_TICKET_FOUND: 'Nenhum Ticket encontrado com este ID.',
-  ERR_NO_USER_FOUND: 'Nenhum usuário encontrado com este ID.',
-  ERR_NO_WAPP_FOUND: 'Nenhum WhatsApp encontrado com este ID.',
-  ERR_CREATING_MESSAGE: 'Erro ao criar mensagem no banco de dados.',
-  ERR_CREATING_TICKET: 'Erro ao criar Ticket no banco de dados.',
-  ERR_COMPANY_NOT_ACTIVE: 'Não foi possivel fazer login empresa Inativa.',
+    'La creación de usuarios ha sido deshabilitada por el administrador.',
+  ERR_NO_PERMISSION: 'No tiene permiso para acceder a este recurso.',
+  ERR_DUPLICATED_CONTACT: 'Ya existe un contacto con este número.',
+  ERR_NO_SETTING_FOUND: 'No se encontró ninguna configuración con este ID.',
+  ERR_NO_CONTACT_FOUND: 'No se encontró ningún contacto con este ID.',
+  ERR_NO_TICKET_FOUND: 'No se encontró ningún ticket con este ID.',
+  ERR_NO_USER_FOUND: 'No se encontró ningún usuario con este ID.',
+  ERR_NO_WAPP_FOUND: 'No se encontró ningún WhatsApp con este ID.',
+  ERR_CREATING_MESSAGE: 'Error al crear el mensaje en la base de datos.',
+  ERR_CREATING_TICKET: 'Error al crear el ticket en la base de datos.',
+  ERR_COMPANY_NOT_ACTIVE: 'No fue posible iniciar sesión. Empresa inactiva.',
   ERR_FETCH_WAPP_MSG:
-    'Erro ao buscar a mensagem no WhatsApp, talvez ela seja muito antiga.'
+    'Error al buscar el mensaje en WhatsApp, puede que sea demasiado antiguo.'
 }
 
 export default backendErrors
